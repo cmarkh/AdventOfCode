dd86cd5d206db19201d7ad62690d371405a853ee
Mark Hendrickson <cmarkh719@gmail.com>
2022-08-06 22:12:39 -0400

    done with day 9

---
+++ go/9/9_test.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	advent "temp/adventofcode/go"
+	"testing"
+
+	"golang.org/x/exp/maps"
+	"golang.org/x/exp/slices"
+)
+
+var testPath = advent.BasePath + "9/test.txt"
+
+func TestInput(t *testing.T) {
+	grid, err := Input(testPath)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	grid.Print(nil)
+}
+
+func TestLowest(t *testing.T) {
+	grid, _ := Input(testPath)
+
+	lowest := grid.LowPoints()
+	grid.Print(lowest)
+
+	fmt.Printf("sum: %d\n", RiskLevel(lowest))
+}
+
+func TestBasins(t *testing.T) {
+	grid, _ := Input(testPath)
+
+	lows := grid.LowPoints()
+	grid.Print(lows)
+
+	basins := grid.Basins(lows)
+
+	for low, b := range basins {
+		fmt.Printf("%v: %v\n", low, b)
+	}
+}
+
+func TestLargestBasins(t *testing.T) {
+	grid, _ := Input(testPath)
+
+	lows := grid.LowPoints()
+	grid.Print(lows)
+
+	basins := grid.Basins(lows)
+
+	basinValues := maps.Values(basins)
+
+	slices.SortFunc(basinValues, func(a, b []Point) bool {
+		return len(a) > len(b)
+	})
+
+	for _, v := range basinValues {
+		fmt.Println(v)
+	}
+
+	product := 1
+	for i, basin := range basinValues {
+		if i == 3 {
+			break
+		}
+		fmt.Println(len(basin))
+		product *= len(basin)
+	}
+
+	fmt.Printf("largest 3 product: %d\n", product)
+}
+
+/*
+func TestFlowBlocked(t *testing.T) {
+	grid, _ := Input(testPath)
+
+	fmt.Println(grid[4][6])
+	fmt.Println(grid.FlowBlocked(Point{3, 4}, Point{4, 6}))
+}
+*/

e0be7f56b190beb26b17396c64f496f5e3dc3838
cmarkh <cmarkh719@gmail.com>
2024-03-23 13:43:41 -0400

    part 1 done

--- 2023/11/day11_rs/src/part1.rs
+++ 2023/11/day11_rs/src/part1.rs
@@ -87,10 +87,57 @@ impl Grid {
             c += 2;
         }
     }
+
+    fn collect_galaxy_coords(&self) -> Vec<(usize, usize)> {
+        let mut galaxies = Vec::new();
+        for (r, row) in self.data.iter().enumerate() {
+            for (c, cell) in row.iter().enumerate() {
+                if cell.is_galaxy {
+                    galaxies.push((r, c));
+                }
+            }
+        }
+        galaxies
+    }
+}
+
+fn shortest_path(a: (usize, usize), b: (usize, usize)) -> usize {
+    let (a_r, a_c) = a;
+    let (b_r, b_c) = b;
+
+    let dist_r = if a_r > b_r { a_r - b_r } else { b_r - a_r };
+    let dist_c = if a_c > b_c { a_c - b_c } else { b_c - a_c };
+
+    dist_r + dist_c
+}
+
+fn pairs(coords: Vec<(usize, usize)>) -> Vec<((usize, usize), (usize, usize))> {
+    let mut pairs = Vec::new();
+
+    for i in 0..coords.len() {
+        for l in (i + 1)..coords.len() {
+            pairs.push((coords[i], coords[l]))
+        }
+    }
+
+    pairs
+}
+
+fn part_1(grid: Grid) -> usize {
+    let galaxies = grid.collect_galaxy_coords();
+    let pairs = pairs(galaxies);
+
+    let mut sum = 0;
+    for pair in &pairs {
+        sum += shortest_path(pair.0, pair.1);
+    }
+
+    sum
 }
 
 #[cfg(test)]
 mod test {
+
     use test_case::case;
 
     use super::*;
@@ -115,5 +162,49 @@ mod test {
         let mut galaxies = get_input(file);
         galaxies.double_empties();
         println!("{}", galaxies);
+    }
+
+    #[ignore]
+    #[case("ex1.txt")]
+    #[case("input.txt")]
+    fn print_galaxies(file: &str) {
+        let mut grid = get_input(file);
+        grid.double_empties();
+        let galaxies = grid.collect_galaxy_coords();
+        for coord in &galaxies {
+            println!("{:?}", coord);
+        }
+        dbg!(&galaxies.len());
+    }
+
+    #[case((0,4), (10,9) => 15)]
+    #[case((2,0), (7,12) => 17)]
+    #[case((7,12), (2,0) => 17)]
+    fn test_shortest_path(a: (usize, usize), b: (usize, usize)) -> usize {
+        let dist = shortest_path(a, b);
+        dbg!(dist)
+    }
+
+    #[ignore]
+    #[case("ex1.txt")]
+    #[case("input.txt")]
+    fn print_pairs(file: &str) {
+        let mut grid = get_input(file);
+        grid.double_empties();
+        let galaxies = grid.collect_galaxy_coords();
+        let pairs = pairs(galaxies);
+        for pair in &pairs {
+            println!("{:?}", pair);
+        }
+        dbg!(pairs.len());
+    }
+
+    #[case("ex1.txt" => 374)]
+    #[case("input.txt" => 10154062)]
+    fn test_part_1(file: &str) -> usize {
+        let mut grid = get_input(file);
+        grid.double_empties();
+        let res = part_1(grid);
+        dbg!(res)
     }
 }

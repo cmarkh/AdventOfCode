8f0c6401f6deeea8a4295bb5e5335e5b5d6c521c
cmarkh <cmarkh719@gmail.com>
2024-12-07 09:31:14 -0500

    day 7 done

---
+++ 2024/day7/src/part1.rs
@@ -0,0 +1,111 @@
+#![allow(dead_code)]
+
+use color_eyre::{eyre::OptionExt, Result};
+
+type Equations = Vec<Equation>;
+
+#[derive(Clone)]
+struct Equation {
+    answer: i64,
+    digits: Vec<i64>,
+}
+
+impl std::fmt::Display for Equation {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}: {:?}", self.answer, self.digits)
+    }
+}
+
+fn read_file(file: &str) -> Result<Equations> {
+    let mut equations = Equations::new();
+
+    let input = std::fs::read_to_string(file)?;
+
+    for line in input.lines() {
+        let (answer, digits) = line.split_once(':').ok_or_eyre("Invalid input")?;
+        let answer: i64 = answer.trim().parse()?;
+        let digits: Vec<i64> = digits
+            .split(' ')
+            .filter(|&s| !s.is_empty())
+            .map(|s| s.parse())
+            .collect::<Result<_, _>>()?;
+        equations.push(Equation { answer, digits });
+    }
+
+    Ok(equations)
+}
+
+fn test_equation(equation: &Equation) -> bool {
+    let mut queue: Vec<(Equation, Vec<char>)> = vec![(equation.clone(), Vec::new())];
+
+    while let Some(item) = queue.pop() {
+        if item.1.len() == item.0.digits.len() - 1 {
+            let mut result = item.0.digits[0];
+            for (i, digit) in item.0.digits.iter().enumerate() {
+                if i == 0 {
+                    continue;
+                }
+                result = operation(result, *digit, item.1[i - 1]);
+            }
+            if result == equation.answer {
+                return true;
+            }
+            continue;
+        }
+
+        let mut item2 = item.clone();
+        item2.1.push('+');
+        queue.push(item2);
+
+        let mut item2 = item.clone();
+        item2.1.push('*');
+        queue.push(item2);
+    }
+
+    false
+}
+
+fn operation(a: i64, b: i64, operator: char) -> i64 {
+    match operator {
+        '+' => a + b,
+        '-' => a - b,
+        '*' => a * b,
+        '/' => a / b,
+        _ => unreachable!("Invalid operator"),
+    }
+}
+
+fn part1(equations: &Equations) -> i64 {
+    let mut sum = 0;
+
+    for equation in equations {
+        if test_equation(equation) {
+            sum += equation.answer;
+        }
+    }
+
+    sum
+}
+
+#[cfg(test)]
+mod tests {
+    use test_case::case;
+
+    use super::*;
+
+    #[case("ex1.txt")]
+    fn test_read_file(file: &str) {
+        let equations = read_file(file).unwrap();
+        for equation in equations {
+            println!("{equation}");
+        }
+    }
+
+    #[case("ex1.txt", 3749)]
+    #[case("input.txt", 4998764814652)]
+    fn test_part1(file: &str, expected: i64) {
+        let equations = read_file(file).unwrap();
+        let result = part1(&equations);
+        assert_eq!(result, expected);
+    }
+}

4b9618fdf8d4a657af6914d233305e76d52847ce
Mark Hendrickson <mhendrickson@veritionfund.com>
2024-12-13 13:20:56 -0500

    day 12 done

--- 2024/day12/src/part2.rs
+++ 2024/day12/src/part2.rs
@@ -15,6 +15,7 @@ fn part2(grid: &Grid) -> usize {
 
     for (r, row) in grid.iter().enumerate() {
         for (c, cell) in row.iter().enumerate() {
+            let mut perimeter = 0;
             let mut queue = vec![(r, c)];
             while let Some(pos) = queue.pop() {
                 if !history.insert(pos) {
@@ -23,68 +24,88 @@ fn part2(grid: &Grid) -> usize {
 
                 groups.entry((*cell, (r, c))).or_default().push(pos);
 
-                // up
-                if pos.0 > 0 && grid[pos.0 - 1][pos.1] == *cell {
-                    queue.push((pos.0 - 1, pos.1));
+                // perimeter
+                {
+                    // upper left
+                    if ((pos.0 == 0 || grid[pos.0 - 1][pos.1] != *cell)
+                        && (pos.1 == 0 || grid[pos.0][pos.1 - 1] != *cell))
+                        || (pos.0 != 0
+                            && pos.1 != 0
+                            && grid[pos.0 - 1][pos.1] == *cell
+                            && grid[pos.0][pos.1 - 1] == *cell
+                            && grid[pos.0 - 1][pos.1 - 1] != *cell)
+                    {
+                        perimeter += 1;
+                    }
+
+                    // upper right
+                    if ((pos.0 == 0 || grid[pos.0 - 1][pos.1] != *cell)
+                        && (pos.1 == row.len() - 1 || grid[pos.0][pos.1 + 1] != *cell))
+                        || (pos.0 != 0
+                            && pos.1 != row.len() - 1
+                            && grid[pos.0 - 1][pos.1] == *cell
+                            && grid[pos.0][pos.1 + 1] == *cell
+                            && grid[pos.0 - 1][pos.1 + 1] != *cell)
+                    {
+                        perimeter += 1;
+                    }
+
+                    // lower left
+                    if ((pos.0 == grid.len() - 1 || grid[pos.0 + 1][pos.1] != *cell)
+                        && (pos.1 == 0 || grid[pos.0][pos.1 - 1] != *cell))
+                        || (pos.0 != grid.len() - 1
+                            && pos.1 != 0
+                            && grid[pos.0 + 1][pos.1] == *cell
+                            && grid[pos.0][pos.1 - 1] == *cell
+                            && grid[pos.0 + 1][pos.1 - 1] != *cell)
+                    {
+                        perimeter += 1;
+                    }
+
+                    // lower right
+                    if ((pos.0 == grid.len() - 1 || grid[pos.0 + 1][pos.1] != *cell)
+                        && (pos.1 == row.len() - 1 || grid[pos.0][pos.1 + 1] != *cell))
+                        || (pos.0 != grid.len() - 1
+                            && pos.1 != row.len() - 1
+                            && grid[pos.0 + 1][pos.1] == *cell
+                            && grid[pos.0][pos.1 + 1] == *cell
+                            && grid[pos.0 + 1][pos.1 + 1] != *cell)
+                    {
+                        perimeter += 1;
+                    }
                 }
 
-                // down
-                if pos.0 < grid.len() - 1 && grid[pos.0 + 1][pos.1] == *cell {
-                    queue.push((pos.0 + 1, pos.1));
-                }
+                // area
+                {
+                    // up
+                    if pos.0 > 0 && grid[pos.0 - 1][pos.1] == *cell {
+                        queue.push((pos.0 - 1, pos.1));
+                    }
 
-                // left
-                if pos.1 > 0 && grid[pos.0][pos.1 - 1] == *cell {
-                    queue.push((pos.0, pos.1 - 1));
-                }
+                    // down
+                    if pos.0 < grid.len() - 1 && grid[pos.0 + 1][pos.1] == *cell {
+                        queue.push((pos.0 + 1, pos.1));
+                    }
 
-                // right
-                if pos.1 < row.len() - 1 && grid[pos.0][pos.1 + 1] == *cell {
-                    queue.push((pos.0, pos.1 + 1));
+                    // left
+                    if pos.1 > 0 && grid[pos.0][pos.1 - 1] == *cell {
+                        queue.push((pos.0, pos.1 - 1));
+                    }
+
+                    // right
+                    if pos.1 < row.len() - 1 && grid[pos.0][pos.1 + 1] == *cell {
+                        queue.push((pos.0, pos.1 + 1));
+                    }
                 }
             }
+
+            if let Some(group) = groups.get(&(*cell, (r, c))) {
+                price += group.len() * perimeter;
+                // println!("{} {:?} {}*{}", grid[r][c], group, group.len(), perimeter);
+            }
+
             // println!("break\n");
         }
-    }
-
-    for group in &mut groups.values_mut() {
-        let mut perimeter = 0;
-
-        let rs = group.iter().map(|(r, _)| r).collect::<HashSet<_>>();
-        for r in rs {
-            dbg!(r);
-            let mut row = group
-                .iter()
-                .filter(|(rr, _)| r == rr)
-                .map(|(_, c)| c)
-                .collect::<Vec<_>>();
-            row.sort();
-            for i in 1..row.len() {
-                if *row[i] != (row[i - 1] + 1) {
-                    perimeter += 1;
-                }
-            }
-            perimeter += 2;
-        }
-        let cs = group.iter().map(|(_, c)| c).collect::<HashSet<_>>();
-        for c in cs {
-            let mut col = group
-                .iter()
-                .filter(|(_, cc)| c == cc)
-                .map(|(r, _)| r)
-                .collect::<Vec<_>>();
-            col.sort();
-            for i in 1..col.len() {
-                if col[i] != col[i - 1] {
-                    perimeter += 1;
-                }
-            }
-            perimeter += 2;
-        }
-
-        dbg!(perimeter);
-
-        price += perimeter * group.len();
     }
 
     price
@@ -112,7 +133,7 @@ mod tests {
     #[case("ex3.txt", 1206)]
     #[case("ex4.txt", 236)]
     #[case("ex5.txt", 368)]
-    #[case("input.txt", 1359028)]
+    #[case("input.txt", 839780)]
     fn test_part2(file: &str, expected: usize) {
         let grid = read_file(file).unwrap();
         let price = part2(&grid);
